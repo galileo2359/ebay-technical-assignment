f2c98153c901671f758a1a85f1110c3b3a19196a
neptune2359 <neptune2359@gmail.com>
2019-07-08 07:07:52 +0100

    Question 3 - DaysOlder.java updated

--- technical-assignment-java/src/main/java/com/assignment/DaysOlder.java
+++ technical-assignment-java/src/main/java/com/assignment/DaysOlder.java
@@ -1,43 +1,71 @@
 package com.assignment;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DaysOlder {
+	
+	public long calculateAge(int year,int month,int day){
+	       // date of birth
+		
+//		year = TWO_YEAR_FORMATTER.parse("99").get(ChronoField.YEAR);
+  //LocalDate pdate = LocalDate.of(1989, 04, 11);
+		year = 1900 + year;
+		LocalDate pdate = LocalDate.of(year, month, day);
+  // current date
+		LocalDate now = LocalDate.now();
+  // difference between current date and date of birth
+  //Period diff = Period.between(pdate, now);
 
+		long noOfDays = ChronoUnit.DAYS.between(pdate, now);
+		
+		return noOfDays;
+	}
+
+	public long retrieveDateField(String name){
+		int countMale=0;
+		long age=0;
+		//Map<String, Long> map1 = new HashMap<String, Long>();
+		//ArrayList<String> list= new ArrayList<String>();//Creating arraylist 
+		
+     try (FileReader reader = new FileReader("C:\\file\\AddressBook");
+             BufferedReader br = new BufferedReader(reader)) {
+
+            // read line by line
+            String line;
+            
+            while ((line = br.readLine()) != null) {
+            	String arr[] = line.split(","); 
+            	
+            	if(arr[0].equals(name)){
+        	    	  age = calculateAge(Integer.parseInt(arr[2].substring(7, 9)), Integer.parseInt(arr[2].substring(4, 6)), 
+          	    			  Integer.parseInt(arr[2].substring(1, 3)));
+            	}
+          	    	  //map1.put(arr[0], age);          
+            	}
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+     return age;
+	}
+	
     public static void main(String[] args) {
-
- //       LocalDate oldDate = LocalDate.of(1982, Month.AUGUST, 31);
- //       LocalDate newDate = LocalDate.of(2016, Month.NOVEMBER, 9);
-
-    	 LocalDate oldDate = LocalDate.of(1982, 07, 31);
-    	 LocalDate newDate = LocalDate.of(2016, 10, 9);
-        System.out.println(oldDate);
-        System.out.println(newDate);
-
-        // count between dates
-        long years = ChronoUnit.YEARS.between(oldDate, newDate);
-        long months = ChronoUnit.MONTHS.between(oldDate, newDate);
-        long weeks = ChronoUnit.WEEKS.between(oldDate, newDate);
-        long days = ChronoUnit.DAYS.between(oldDate, newDate);
- /*       long hours = ChronoUnit.HOURS.between(oldDate, newDate);
-        long minutes = ChronoUnit.MINUTES.between(oldDate, newDate);
-        long seconds = ChronoUnit.SECONDS.between(oldDate, newDate);
-        long milis = ChronoUnit.MILLIS.between(oldDate, newDate);
-        long nano = ChronoUnit.NANOS.between(oldDate, newDate);*/
-
-        System.out.println("\n--- Total --- ");
-        System.out.println(years + " years");
-        System.out.println(months + " months");
-        System.out.println(weeks + " weeks");
-        System.out.println(days + " days");
-      /*  System.out.println(hours + " hours");
-        System.out.println(minutes + " minutes");
-        System.out.println(seconds + " seconds");
-        System.out.println(milis + " milis");
-        System.out.println(nano + " nano");*/
-
+    	//Map<String, Long> map1 = new HashMap<String, Long>();//Creating arraylist 
+    	DaysOlder obj = new DaysOlder();
+    	
+ 	   	long age1=obj.retrieveDateField("Bill McKnight");
+ 		long age2=obj.retrieveDateField("Paul Robinson");
+ 	   	
+ 	   	//Bill McKnight  Paul Robinson
+ 		
+ 		System.out.println("The age difference is :"+(age1-age2));
     }
 }
