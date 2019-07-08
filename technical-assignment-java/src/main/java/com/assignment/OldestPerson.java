9d3e6f9406cd29cefbb6935d2b5fb1a9101de368
neptune2359 <neptune2359@gmail.com>
2019-07-08 05:54:38 +0100

    OldestPerson.java modified

--- technical-assignment-java/src/main/java/com/assignment/OldestPerson.java
+++ technical-assignment-java/src/main/java/com/assignment/OldestPerson.java
@@ -11,10 +11,13 @@ public class OldestPerson {
 	
 	public long calculateAge(int year,int month,int day){
 	       // date of birth
+		
+//		year = TWO_YEAR_FORMATTER.parse("99").get(ChronoField.YEAR);
      //LocalDate pdate = LocalDate.of(1989, 04, 11);
+		year = 1900 + year;
 		LocalDate pdate = LocalDate.of(year, month, day);
      // current date
-     LocalDate now = LocalDate.now();
+		LocalDate now = LocalDate.now();
      // difference between current date and date of birth
      //Period diff = Period.between(pdate, now);
 
@@ -23,69 +26,86 @@ public class OldestPerson {
 		return noOfDays;
 	}
 	
-	public ArrayList<String> retrieveDateField(){
+	public Map<String, Long> retrieveDateField(){
 		int countMale=0;
-		ArrayList<String> list= new ArrayList<String>();//Creating arraylist 
+		Map<String, Long> map1 = new HashMap<String, Long>();
+		//ArrayList<String> list= new ArrayList<String>();//Creating arraylist 
 		
      try (FileReader reader = new FileReader("C:\\file\\AddressBook");
              BufferedReader br = new BufferedReader(reader)) {
 
             // read line by line
             String line;
+            long age=0;
             while ((line = br.readLine()) != null) {
             	String arr[] = line.split(","); 
-                
-            	//System.out.println(line);
-            	
-            	for (String temp: arr){
-            		  temp = temp.trim();
-
-          	      if(temp.charAt(2)=='/'){
-          	    	  countMale = countMale + 1;
-          	    	  list.add(temp);
-          	      }
-            		//System.out.println(temp);
-          	    // String arr1[] = temp.split("/");
-          
+               
+          	    	  age = calculateAge(Integer.parseInt(arr[2].substring(7, 9)), Integer.parseInt(arr[2].substring(4, 6)), 
+          	    			  Integer.parseInt(arr[2].substring(1, 3)));
+          	    	  map1.put(arr[0], age);          
             	}
-            }
-           
-           // System.out.println(countMale);
-            
-         
 
         } catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
-     return list;
+     return map1;
 	}
 	
+	public Map<String, Long> compareAge(Map<String, Long> map1){
+		//Map<String, String> map1 = new HashMap<String, String>();
+		  for (Map.Entry<String, Long> entry : map1.entrySet()) {
+			//	System.out.println("Name :"+entry.getKey()+"\tDate :"+entry.getValue());
+			  
+				
+				//if(entry.getValue()==)
+			}
+		
+		return map1;
+	}
 	
+	public HashMap<String, Long> sortByValue(Map<String, Long> map1) 
+    { 
+        // Create a list from elements of HashMap 
+        List<Map.Entry<String, Long> > list = 
+               new LinkedList<Map.Entry<String, Long> >(map1.entrySet()); 
+  
+        // Sort the list 
+        Collections.sort(list, new Comparator<Map.Entry<String, Long> >() { 
+            public int compare(Map.Entry<String, Long> o1,  
+                               Map.Entry<String, Long> o2) 
+            { 
+                return (o1.getValue()).compareTo(o2.getValue()); 
+            } 
+        }); 
+          
+        // put data from sorted list to hashmap  
+        HashMap<String, Long> temp = new LinkedHashMap<String, Long>(); 
+        for (Map.Entry<String, Long> aa : list) { 
+            temp.put(aa.getKey(), aa.getValue()); 
+        } 
+        return temp; 
+    } 
+  
 	
-public static void main(String[] args)
- {
-	   ArrayList<String> list=new ArrayList<String>();//Creating arraylist 
+public static void main(String[] args){
+	Map<String, Long> map1 = new HashMap<String, Long>();//Creating arraylist 
 	   OldestPerson obj = new OldestPerson();
-	   list = obj.retrieveDateField();
+	   map1=obj.retrieveDateField();
+//	   System.out.println("Result "+obj.calculateAge(94, 4, 10));
 	   
-	   for(int i=0;i<list.size();i++){
- 	   System.out.println(list.get(i));
-    }
+//	   for(int i=0;i<list.size();i++){
+// 	   System.out.println(list.get(i));
+//    }
+	   Map<String, Long> hm1 = obj.sortByValue(map1); 
+	
+	   
+	   
+	  for (Map.Entry<String, Long> entry : hm1.entrySet()) {
+			System.out.println("Name :"+entry.getKey()+"\tDate :"+entry.getValue());
+			
+			//if(entry.getValue()==)
+		}
 	   //System.out.println("The total number of "+list.countField());
 	   
-/*      
-     // date of birth
-     LocalDate pdate = LocalDate.of(1989, 04, 11);
-     // current date
-     LocalDate now = LocalDate.now();
-     // difference between current date and date of birth
-     Period diff = Period.between(pdate, now);
-     
-
-  System.out.printf("\nI am  %d years, %d months and %d days old.\n\n", 
-                 diff.getYears(), diff.getMonths(), diff.getDays());
-  
-  System.out.print(diff.getDays());
-  */
 }
 }
